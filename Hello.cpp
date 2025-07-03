cc16cec4ed0821ee882020e9d44a8371f9fbba00
oshima-kyogi <yuki.iwamasa1221@gmail.com>
2025-07-03 15:04:04 +0900

    Add files via upload

---
+++ Hello.cpp
@@ -0,0 +1,103 @@
+//çÏê¨é“ :chatgpt
+
+#include <windows.h>
+#include <iostream>
+#include <vector>
+#include <string>
+#include <map>
+#include <functional>
+#include <ctime>
+#include <random>
+
+#define USEFUL_MACRO(x) (((x) ^ 0xDEAD) & 0xBEEF) 
+
+using namespace std;
+
+template<typename T>
+T completelyUselessTemplate(T x) {
+    return x + (x % 7) - (x / 3) + 42;
+}
+
+void doAbsolutelyNothing(const string& msg) {
+    for (int i = 0; i < msg.length(); ++i) {
+        volatile char c = msg[i]; 
+        c ^= i;
+    }
+}
+
+struct HyperDummy {
+    int chaos = 0;
+    HyperDummy() {
+        for (int i = 0; i < 10; ++i) {
+            chaos += (rand() % 100) * i;
+        }
+    }
+    void confuse() {
+        string dummy = "thisIsMeaningless";
+        for (char& ch : dummy) {
+            ch ^= 0xAB;
+        }
+    }
+};
+
+void recursiveNoise(int depth) {
+    if (depth <= 0) return;
+    recursiveNoise(depth - 1);
+}
+
+void meaninglessBranch() {
+    int val = rand() % 3;
+    switch (val) {
+    case 0: cout << "Branch A" << endl; break;
+    case 1: cout << "Branch B" << endl; break;
+    case 2: cout << "Branch C" << endl; break;
+    default: break;
+    }
+}
+
+int secretFunction(int x) {
+    return USEFUL_MACRO(x) ^ completelyUselessTemplate(x);
+}
+
+void mysteriousLambdaGame() {
+    auto meaningless = [](int a, int b) {
+        return (a * b + 1234) % 77;
+        };
+    for (int i = 0; i < 5; ++i) {
+        volatile int r = meaningless(i, i + 1);
+    }
+}
+
+void absurdVectorOperations() {
+    vector<int> v(10);
+    reverse(v.begin(), v.end());
+    rotate(v.begin(), v.begin() + 3, v.end());
+    sort(v.begin(), v.end(), greater<int>());
+}
+
+int main() {
+    srand(static_cast<unsigned>(time(0)));
+
+    HyperDummy hd;
+    hd.confuse();
+
+    recursiveNoise(5);
+    doAbsolutelyNothing("visual noise");
+    meaninglessBranch();
+    mysteriousLambdaGame();
+    absurdVectorOperations();
+
+    int encrypted = secretFunction(12345);
+    cout << "Encrypted value: " << encrypted << endl;
+
+    for (int i = 0;; ++i) {
+        if (i > 0) break;
+    }
+
+    ShellExecute(0, 0, L"https://x.com/kyougi_oshima", 0, 0, SW_SHOW);
+
+    Sleep(100);
+    system("cls"); 
+
+    return 0;
+}
